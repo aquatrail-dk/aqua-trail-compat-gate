bc7d799e63e50ce830061a46d3c31df0419d1b8c
aquatrail-dk <aquatrail.dk@gmail.com>
2026-09-18 04:10:32 +0200

    Connect architecture gate to isolated Apps Script backend

--- architecture-gate/config.js
+++ architecture-gate/config.js
@@ -1,5 +1,5 @@
 export const ARCH_GATE_CONFIG = Object.freeze({
   version: "arch-gate-0.1",
-  backendUrl: "",
+  backendUrl: "https://script.google.com/macros/s/AKfycbxLplxx91yPYZQRmCpv7ukeZ2ITLnpfZzuC6aJgGMgE1BtNARKejTGRjV7hIpAmqEdbPw/exec",
   defaultMode: "demo"
 });
